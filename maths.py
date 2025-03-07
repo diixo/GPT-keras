3914adb3cb0525be43a4f3caf9d71224b20738fc
diixo <diixo.com@gmail.com>
2025-03-07 05:29:45 +0200

    maths.py version 4: implement new self-attention (k,q,v)

--- maths.py
+++ maths.py
@@ -2,6 +2,8 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from keras import layers
+
 
 tf.random.set_seed(2081)
 
@@ -61,3 +63,34 @@ wei = tf.nn.softmax(wei, axis=-1)
 xbow3 = tf.matmul(wei, x)  # (T,T) @ (B,T,C) --> (B,T,C)
 
 print(np.isclose(xbow, xbow3, atol=1e-6))   # --> True
+
+
+################################################################################
+# version 4: implement self attention
+B, T, C = 4, 8, 32  # (Batches, Times, Channels)
+x = tf.random.normal(shape=(B, T, C))
+
+# functionality of single Head perform the self-attention
+head_size = 16
+
+key = layers.Dense(head_size, use_bias=False)   # out_features=head_size
+query = layers.Dense(head_size, use_bias=False) # out_features=head_size
+value = layers.Dense(head_size, use_bias=False) # out_features=head_size
+
+k = key(x)      # (B, T, head_size)
+q = query(x)    # (B, T, head_size)
+k_T = tf.transpose(k, perm=[0, 2, 1])   # (B, T, head_size) --> (B, head_size, T)
+wei = tf.matmul(q, k_T)                 # (B, T, head_size) @ (B, head_size, T) --> (B, T, T)
+
+
+tril = tf.linalg.band_part(tf.ones((T, T)), -1, 0)
+#wei = tf.zeros((T, T))
+wei = tf.where(tril[:T, :T] == 0, float('-inf'), wei)
+wei = tf.nn.softmax(wei, axis=-1)
+print(wei.numpy())
+
+# x is private information of token
+v = value(x)
+xbow3 = tf.matmul(wei, v)  # (T, T) @ (B, T, head_size) --> (B, T, head_size)
+print(xbow3.shape)
+# print(xbow3.numpy())
