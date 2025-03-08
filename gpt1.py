2237ed576bf8ef7c3de53d11f49f63aae058affe
diixo <diixo.com@gmail.com>
2025-03-08 05:16:23 +0200

    added multiheading

--- gpt1.py
+++ gpt1.py
@@ -32,16 +32,24 @@ train_data = data[:n]
 val_data = data[n:]
 
 
+# def get_batch(split):
+#     data = train_data if split == 'train' else val_data
+#     ix = tf.random.uniform((batch_size,), maxval=len(data) - block_size, dtype=tf.int64)
+#     x = tf.gather(data, ix[:, tf.newaxis] + tf.cast(tf.range(block_size), dtype=tf.int64))
+#     y = tf.gather(data, ix[:, tf.newaxis] + tf.cast(tf.range(1, block_size + 1), dtype=tf.int64))
+#     x, y = tf.convert_to_tensor(x, dtype=tf.int64), tf.convert_to_tensor(y, dtype=tf.int64)
+#     return x, y
+
+
 def get_batch(split):
-    data = train_data if split == 'train' else val_data
-    ix = tf.random.uniform((batch_size,), maxval=len(data) - block_size, dtype=tf.int64)
-    x = tf.gather(data, ix[:, tf.newaxis] + tf.cast(tf.range(block_size), dtype=tf.int64))
-    y = tf.gather(data, ix[:, tf.newaxis] + tf.cast(tf.range(1, block_size + 1), dtype=tf.int64))
-    x, y = tf.convert_to_tensor(x, dtype=tf.int64), tf.convert_to_tensor(y, dtype=tf.int64)
+    data_split = train_data if split == 'train' else val_data
+    ix = tf.random.uniform(shape=(batch_size,), maxval=len(data_split) - block_size, dtype=tf.int32)
+    x = tf.stack([data_split[i:i+block_size] for i in ix])
+    y = tf.stack([data_split[i+1:i+block_size+1] for i in ix])
     return x, y
 
 
-@tf.function
+#@tf.function
 def estimate_loss():
     out = {}
     model.trainable = False
@@ -89,6 +97,18 @@ class Head(tf.keras.layers.Layer):
         return out
 
 
+class MultiHeadAttention(tf.keras.layers.Layer):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super(MultiHeadAttention, self).__init__()
+        self.heads = [Head(head_size) for _ in range(num_heads)]
+
+    def call(self, x):
+        out = tf.concat([h(x) for h in self.heads], axis=-1)
+        return out
+
+
 class BigramLanguageModel(keras.Model):
 
     def __init__(self, vocab_size):
@@ -96,7 +116,7 @@ class BigramLanguageModel(keras.Model):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = layers.Embedding(vocab_size, n_embd)
         self.position_embedding_table = layers.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimensional self-attention (32)
         self.lm_head = layers.Dense(units=vocab_size, input_shape=(n_embd,))
 
 
@@ -107,7 +127,7 @@ class BigramLanguageModel(keras.Model):
         tok_emb = self.token_embedding_table(idx)               # (B, T, C=n_embd)
         pos_emb = self.position_embedding_table(tf.range(T))    # (T, C=n_embd)
         x = tok_emb + pos_emb               # (B, T, C)
-        x = self.sa_head(x)                 # apply one head of self-attention (B, T, C)
+        x = self.sa_heads(x)                # apply one head of self-attention (B, T, C)
         logits = self.lm_head(x)            # (B, T, vocab_sz)
 
         if targets is None:
