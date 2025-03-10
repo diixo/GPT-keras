aae61a35fab12fa2a1007a9b6d924c92ffe45412
diixo <diixo.com@gmail.com>
2025-03-10 11:39:52 +0200

    3_gpt_pt.py coding style

--- 3_gpt_pt.py
+++ 3_gpt_pt.py
@@ -92,13 +92,11 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
-        #self.proj = nn.Linear(head_size * num_heads, n_embd)
         self.proj = nn.Linear(n_embd, n_embd)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.proj(out)
-        return out
+        return self.proj(out)
 
 
 class FeedForward(nn.Module):
@@ -171,6 +169,7 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
+            # crop idx to the last block_size token
             idx_cond = idx[:, -block_size:]
             # get the predictions
             logits, loss = self(idx_cond)
