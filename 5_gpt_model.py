6030f7447654ab21bac77ddc22391e42d95b846a
diixo <diixo.com@gmail.com>
2025-03-15 01:14:22 +0200

    fixed dimention-s errors

--- 5_gpt_model.py
+++ 5_gpt_model.py
@@ -4,9 +4,10 @@ from keras import layers
 import numpy as np
 
 
-# Hyperparameters
+# ---------- hyperparameters ----------
 batch_size = 32 # amount independent sequences will we process in parallel
-block_size = 64 # maximum context length for predictions
+block_size = 80 # maximum context length for predictions
+
 max_iters = 5000
 eval_interval = 1000
 learning_rate = 1e-3
@@ -15,7 +16,7 @@ eval_iters = 200
 n_embd = 256
 n_head = 4
 n_layer = 4
-dropout = 0.2
+dropout_rate = 0.2
 
 tf.random.set_seed(1337)
 
@@ -32,37 +33,43 @@ itos = {i: ch for i, ch in enumerate(chars)}
 encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: ''.join([itos[i] for i in l])
 
-data = encode(text)
+data = tf.constant(encode(text), dtype=tf.int64)
 n = int(0.9 * len(data))
-train_data = np.array(data[:n])
-val_data = np.array(data[n:])
+train_data = data[:n]
+val_data = data[n:]
 
 
 # data loading
 def get_batch(split):
     data_split = train_data if split == 'train' else val_data
-    ix = np.random.randint(0, len(data_split) - block_size, size=(batch_size,))
-    x = np.stack([data_split[i:i+block_size] for i in ix])
-    y = np.stack([data_split[i+1:i+block_size+1] for i in ix])
+    ix = tf.random.uniform(shape=(batch_size,), maxval=len(data_split) - block_size, dtype=tf.int32)
+    x = tf.stack([data_split[i:i+block_size] for i in ix])
+    y = tf.stack([data_split[i+1:i+block_size+1] for i in ix])
     return x, y
 
 
-class Head(tf.keras.layers.Layer):
+class Head(layers.Layer):
     """ one head of self-attention """
 
-    def __init__(self, head_size):
+    def __init__(self, head_size, dropout_rate=0.2):
         super().__init__()
-        self.key = layers.Dense(input_shape=(n_embd,), units=head_size, use_bias=False)
-        self.query = layers.Dense(input_shape=(n_embd,), units=head_size, use_bias=False)
-        self.value = layers.Dense(input_shape=(n_embd,), units=head_size, use_bias=False)
         self.head_size = head_size
+        self.dropout_rate = dropout_rate
+
+    def build(self, input_shape):
+        assert(block_size == input_shape[1])
+        self.key   = layers.Dense(units=self.head_size, use_bias=False) # (n_embd, head_size)
+        self.query = layers.Dense(units=self.head_size, use_bias=False) # (n_embd, head_size)
+        self.value = layers.Dense(units=self.head_size, use_bias=False) # (n_embd, head_size)
 
         tril = tf.linalg.band_part(tf.ones((block_size, block_size)), -1, 0)
         self.tril = tf.constant(tril)
-        self.dropout = layers.Dropout(dropout)
+        self.dropout = layers.Dropout(self.dropout_rate)
+
+        super().build(input_shape)
 
 
-    def call(self, x, training=False):
+    def call(self, x):
         # input of size (batch, time-step, channels)
         # output of size (batch, time-step, head_size)
         B, T, C = x.shape
@@ -75,7 +82,7 @@ class Head(tf.keras.layers.Layer):
         wei = tf.matmul(q, k_T) * scale         # (B, T, head_size) @ (B, head_size, T) --> (B, T, T)
         wei = tf.where(self.tril[:T, :T] == 0, float('-inf'), wei)  # (B, T, T)
         wei = tf.nn.softmax(wei, axis=-1)       # (B, T, T)
-        wei = self.dropout(wei, training=training)
+        wei = self.dropout(wei)
 
         # perform the weighted aggregation of the values
         v = self.value(x)                       # (B, T, head_size)
@@ -85,14 +92,19 @@ class Head(tf.keras.layers.Layer):
         return out
 
 
-class MultiHeadAttention(tf.keras.layers.Layer):
+class MultiHeadAttention(layers.Layer):
     """ multiple heads of self-attention in parallel """
 
     def __init__(self, num_heads, head_size):
         super().__init__()
-        self.heads = [Head(head_size) for _ in range(num_heads)]
-        self.proj = layers.Dense(input_shape=(head_size*num_heads,), units=n_embd)  # head_size * num_heads, n_embd
-        self.dropout = layers.Dropout(dropout)
+        self.num_heads = num_heads
+        self.head_size = head_size
+
+    def build(self, input_shape):
+        self.heads = [Head(self.head_size) for _ in range(self.num_heads)]
+        self.proj = layers.Dense(units=n_embd)  # (head_size * num_heads, n_embd)
+        self.dropout = layers.Dropout(dropout_rate)
+        super().build(input_shape)
 
     def call(self, x, training=False):
         out = tf.concat([h(x) for h in self.heads], axis=-1)
@@ -102,18 +114,21 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         return out
 
 
-class FeedForward(tf.keras.layers.Layer):
+class FeedForward(layers.Layer):
     """ a simple linear layer followed by a non-linearity """
 
     def __init__(self, n_embd):
         super().__init__()
-        self.net = tf.keras.Sequential([
-            layers.Dense(input_shape=(n_embd,), units=4*n_embd),
-            layers.ReLU(),
-            layers.Dropout(dropout),
-            layers.Dense(input_shape=(4*n_embd,), units=n_embd),
-        ])
         self.n_embd = n_embd
+
+    def build(self, input_shape):
+        self.net = tf.keras.Sequential([
+            layers.Dense(units=4*self.n_embd),  # (n_embd, 4*n_embd)
+            layers.ReLU(),
+            layers.Dropout(dropout_rate),
+            layers.Dense(units=self.n_embd),    # (4*n_embd, n_embd)
+        ])
+        super().build(input_shape)
 
     def call(self, x, training=False):
         out = self.net(x, training=training)   # B, T, n_embd
@@ -121,24 +136,29 @@ class FeedForward(tf.keras.layers.Layer):
         return out
 
 
-class Block(tf.keras.layers.Layer):
+class Block(layers.Layer):
     """ Transformer block: communication followed by computation """
 
-    def __init__(self, n_embd, n_head, dropout_rate=0.2):
+    def __init__(self, n_embd, n_head):
         super().__init__()
-        head_size = n_embd // n_head
-        self.sa = MultiHeadAttention(n_head, head_size)
-        self.ffwd = FeedForward(n_embd)
+        self.head_size = n_embd // n_head
+        self.n_head = n_head
+        self.n_embd = n_embd
+
+    def build(self, input_shape):
+        self.sa = MultiHeadAttention(self.n_head, self.head_size)
+        self.ffwd = FeedForward(self.n_embd)
         self.ln1 = layers.LayerNormalization(epsilon=1e-6)
         self.ln2 = layers.LayerNormalization(epsilon=1e-6)
 
         self.dropout_sa = layers.Dropout(dropout_rate)
         self.dropout_ffn = layers.Dropout(dropout_rate)
+        super().build(input_shape)
 
     def call(self, x, training=False):
         # Pre-LN: normalization before MHA
-        x = x + self.dropout_sa(self.sa(self.ln1(x)), training=training)     # dropout output only MHA
-        x = x + self.dropout_ffn(self.ffwd(self.ln2(x)), training=training)  # dropout output only FFN
+        x = x + self.dropout_sa(self.sa(self.ln1(x)))     # dropout output only MHA
+        x = x + self.dropout_ffn(self.ffwd(self.ln2(x)))  # dropout output only FFN
         return x
 
 
@@ -147,33 +167,44 @@ class BigramLanguageLayer(layers.Layer):
     def __init__(self, vocab_size, n_embd, n_head, n_block, dropout_rate=0.2):
         super().__init__()
 
+        self.vocab_size = vocab_size
+        self.n_embd = n_embd
+        self.n_head = n_head
+        self.dropout_rate = dropout_rate
+        self.n_block = n_block
+
+    def build(self, input_shape):
+        assert(block_size == input_shape[1])
+        self.block_size = input_shape[1]
+
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = layers.Embedding(vocab_size, n_embd)
-        self.position_embedding_table = layers.Embedding(block_size, n_embd)
-        self.blocks = tf.keras.Sequential([Block(n_embd, n_head, dropout_rate) for _ in range(n_block)])
+        self.token_embedding_table    = layers.Embedding(self.vocab_size, self.n_embd)
+        self.position_embedding_table = layers.Embedding(self.block_size, self.n_embd)
+        self.blocks = keras.Sequential([Block(self.n_embd, self.n_head) for _ in range(self.n_block)])
         self.ln_f = layers.LayerNormalization(epsilon=1e-6) # final layer norm
-        self.lm_head = layers.Dense(input_shape=(n_embd,), units=vocab_size)
+        self.lm_head = layers.Dense(units=self.vocab_size)  # (n_embd, vocab_size)
+        super().build(input_shape)
 
 
     def call(self, idx, targets=None):
         B, T = idx.shape
+        assert(self.block_size == T)
 
         # idx and targets are both (B=batch, T=time) tensor of integers
         tok_emb = self.token_embedding_table(idx)               # (B, T, C=n_embd)
-        pos_emb = self.position_embedding_table(tf.range(T))    # (T, C=n_embd)
+        pos_emb = self.position_embedding_table(tf.range(0, T)) # (T, C=n_embd)
         x = tok_emb + pos_emb               # (B, T, C)
         x = self.blocks(x)                  # (B, T, C)
         x = self.ln_f(x)                    # (B, T, C)
         logits = self.lm_head(x)            # (B, T, vocab_sz)
-
         if targets is None:
-            loss = None
+            return logits
         else:
             B, T, C = logits.shape
             logits = tf.reshape(logits, (B * T, C))
             targets = tf.reshape(targets, (B * T,))
             loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=targets, logits=logits))
-        return logits, loss
+            return logits, loss
 
 
     def generate(self, idx, max_new_tokens):
@@ -195,15 +226,19 @@ class BigramLanguageLayer(layers.Layer):
 
 
 class TransformerModel:
-    def __init__(self, vocab_size, n_embd, n_head, n_layer, block_size, dropout_rate, learning_rate):
+    def __init__(self, vocab_size, n_embd, n_head, n_layer, block_size, dropout_rate, learning_rate, random_seed=2081):
         self.block_size = block_size
         self.vocab_size = vocab_size
 
-        keras.utils.set_random_seed(2081)
+        keras.utils.set_random_seed(random_seed)
         inputs = keras.Input((block_size,), dtype="int32")
         outputs = BigramLanguageLayer(vocab_size, n_embd, n_head, n_layer, dropout_rate)(inputs)
         self.model = keras.Model(inputs, outputs)
 
+        # for var in self.model.trainable_variables:
+        #     print(f"--- {var.name}: {var.shape}")
+
+        # keras.optimizers.experimental.AdamW behaves strangely. Using Adam instead for now.
         self.model.compile(
             optimizer=tf.optimizers.AdamW(
                 learning_rate=learning_rate,
@@ -234,7 +269,7 @@ class TransformerModel:
 def train_model():
     """ return TransformerModel """
 
-    model = TransformerModel(vocab_size, n_embd, n_head, n_layer, block_size, dropout, learning_rate)
+    model = TransformerModel(vocab_size, n_embd, n_head, n_layer, block_size, dropout_rate, learning_rate)
 
     for iter in tf.range(max_iters):
 
@@ -243,8 +278,10 @@ def train_model():
         model.train_on_batch(Xb, Yb)
 
         if iter % eval_interval == 0:
-            losses = model.estimate_loss()
-            print(f"...on {iter.numpy()}(th): train_loss({losses['train']:.4f}), val_loss({losses['val']:.4f})")
+            #losses = model.estimate_loss()
+            losses = None
+            if losses:
+                print(f"...on {iter.numpy()}(th): train_loss({losses['train']:.4f}), val_loss({losses['val']:.4f})")
         else:
             if (iter % 100 == 0) and (iter > 0):
                 print(f"...on {iter}(th) epoch...")
@@ -256,9 +293,9 @@ def train_model():
     return model
 
 
-model = train_model()
-
 # Generate text from the model
 idx = tf.zeros((1, 1), dtype=tf.int64)  # (B, T)
+
+model = train_model()
 generated_text = decode(model.generate(idx, max_new_tokens=500).numpy()[0])
 print(generated_text)
