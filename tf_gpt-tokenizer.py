5f0aa2ee3caf85ab4d3a0498a434a63fab0173ff
diixo <diixo.com@gmail.com>
2025-03-22 09:41:54 +0200

    tf_gpt-tokenizer.py: clean-up

--- tf_gpt-tokenizer.py
+++ tf_gpt-tokenizer.py
@@ -23,24 +23,31 @@ with open('input.txt', 'r', encoding='utf-8') as f:
 
 tokens = tokenizer(text, add_special_tokens=False, truncation=False)["input_ids"]
 
+print(len(tokens))
+
 vocab_size = tokenizer.vocab_size
 
-def create_sequences(tokenized_text, seq_length):
-    X, Y = [], []
-    for i in range(len(tokenized_text) - seq_length):
-        X.append(tokenized_text[i: i + seq_length])
-        Y.append(tokenized_text[i + 1: i + seq_length + 1])
-    return np.array(X), np.array(Y)
+# def create_sequences(tokenized_text, seq_length):
+#     X, Y = [], []
+#     for i in range(len(tokenized_text) - seq_length):
+#         X.append(tokenized_text[i: i + seq_length])
+#         Y.append(tokenized_text[i + 1: i + seq_length + 1])
+#     return np.array(X), np.array(Y)
 
 
-X, Y = create_sequences(tokens, seq_length)
+# X, Y = create_sequences(tokens, seq_length)
 
-steps_per_epoch = len(X) // batch_size
+steps_per_epoch = len(tokens) // (batch_size * (seq_length + 1))  # количество батчей на одну эпоху
 print(f"Количество батчей в эпохе: {steps_per_epoch}")
 
 #########################################################
 
-dataset = (tf.data.Dataset.from_tensor_slices((X, Y))
+def split_input_target(chunk):
+    return chunk[:-1], chunk[1:]
+
+dataset = (tf.data.Dataset.from_tensor_slices(tokens)
+           .batch(seq_length + 1, drop_remainder=True)
+           .map(split_input_target)
            .shuffle(10000)
            .batch(batch_size, drop_remainder=True))
 
@@ -70,7 +77,8 @@ def generate_text(model, start_string, length=50):
 
     for _ in range(length):
         predictions = model(input_ids).logits
-        predicted_id = tf.random.categorical(predictions[:, -1, :], num_samples=1).numpy()[0, 0]
+        predictions = predictions[:, -1, :]
+        predicted_id = tf.random.categorical(predictions, num_samples=1).numpy()[0, 0]
         input_ids = tf.concat([input_ids, [[predicted_id]]], axis=-1)
 
     return tokenizer.decode(input_ids.numpy()[0])
