9fcf18c72283b1c00e600919744210a71cd23e7b
diixo <diixo.com@gmail.com>
2025-03-24 23:06:20 +0200

    tf_autotokenizer.py: separated

--- tf_autotokenizer.py
+++ tf_autotokenizer.py
@@ -1,16 +1,21 @@
+import tensorflow as tf
 from transformers import AutoTokenizer
 
 
+batch_size = 1
 tokenizer = AutoTokenizer.from_pretrained("all-MiniLM-L6-v2")
 
 text = ["This is a sentence viiX.", "viiX is about of IT", "Here is another sentence, that is a bit longer."]
 
 # add_special_tokens=True (by default): [CLS]=101, [SEP]=102...
-tokens = tokenizer(text, add_special_tokens=False, truncation=False, padding=True, return_tensors="np")["input_ids"] 
+tokens = tokenizer(text, add_special_tokens=False, truncation=False, padding=True, return_tensors="np")
 
-print(tokens)
-print("max_id:", tokens.max())
-print("max_sz:", tokens.shape[-1])
+input_ids = tokens["input_ids"]
+attention_mask = tokens["attention_mask"]
 
-decoded_texts = [tokenizer.decode(seq, skip_special_tokens=True) for seq in tokens]
+print(input_ids)
+print("max_id:", input_ids.max())
+print("max_sz:", input_ids.shape[-1])
+
+decoded_texts = [tokenizer.decode(seq, skip_special_tokens=True) for seq in input_ids]
 print(decoded_texts)
