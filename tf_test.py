9368cdac8001ecef9595909f219139c670dfbb98
diixo <diixo.com@gmail.com>
2025-03-25 07:48:49 +0200

    tf_test.py: updated

--- tf_test.py
+++ tf_test.py
@@ -1,6 +1,12 @@
 from transformers import GPT2Tokenizer, TFGPT2LMHeadModel, GPT2Config
 import tensorflow as tf
+import re
 
+
+def str_tokenize_words(s: str, stopwords=set()):
+    words = re.findall("(\.?\w[\w'\.&]*\w|\w\+*#?)", s)
+    if words: return [w for w in words if w not in stopwords]
+    return []
 
 batch_size = 32
 seq_length = 32
@@ -9,20 +15,25 @@ dff = 256
 num_heads = 4
 num_layers = 4
 
-# 1. Загрузка данных из текстового файла
-file_path = 'input.txt'  # Путь к вашему текстовому файлу
+epochs = 3
+
+
+model_path = f"shakespeare-gpt2-{embedding_dim}-{batch_size}-{seq_length}-{dff}-{num_heads}.h5"
+
+file_path = 'input.txt'
 with open(file_path, 'r', encoding='utf-8') as file:
-    texts = file.read().splitlines()  # Разделяем на строки
+    texts = file.read().splitlines()
 
-# 2. Токенизация
+lines = [line for line in texts if len(str_tokenize_words(line)) > 1]
+
+
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-tokenizer.pad_token = tokenizer.eos_token  # Устанавливаем паддинг токен в качестве EOS
-
-# Токенизируем все строки в файле
-encodings = tokenizer(texts, padding=True, truncation=True, return_tensors='tf', max_length=seq_length)
+tokenizer.pad_token = tokenizer.eos_token
 
 
-# 3. Создание модели с нуля
+encodings = tokenizer(lines, padding=True, truncation=True, return_tensors='tf', max_length=seq_length)
+
+
 config = GPT2Config(
     vocab_size=tokenizer.vocab_size, 
     n_positions=seq_length,
@@ -33,35 +44,27 @@ config = GPT2Config(
 )
 model = TFGPT2LMHeadModel(config)
 
-# 4. Подготовка данных для обучения
-# Для обучения модели необходимо подготовить входные и целевые данные.
-# В нашем случае входные данные такие же, как и целевые (предсказание следующего токена).
 inputs = encodings['input_ids']
 labels = encodings['input_ids']
+attention_mask = encodings['attention_mask']
 
-# Преобразуем их в tf.data.Dataset
-dataset = tf.data.Dataset.from_tensor_slices((inputs, labels))
 
-# Используем батчирование для обучения
-batch_size = 2
+dataset = tf.data.Dataset.from_tensor_slices((inputs, labels, attention_mask))
+
 dataset = dataset.shuffle(1000).batch(batch_size, drop_remainder=True)
 
-# 5. Определение оптимизатора
-optimizer = tf.keras.optimizers.Adam(learning_rate=5e-5)
 
-# 6. Определение функции потерь
+optimizer = tf.keras.optimizers.AdamW(learning_rate=5e-4)
+
+
 def compute_loss(labels, logits):
     loss = tf.reduce_mean(tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True))
     return loss
 
-# 7. Обучение модели с использованием model.fit()
-# Для использования model.fit() в Keras, нужно определить модель, оптимизатор и метрики.
 model.compile(optimizer=optimizer, loss=compute_loss)
 
-# 8. Обучение модели
-epochs = 3  # Количество эпох
 model.fit(dataset, epochs=epochs)
 
-# Сохранение модели после обучения
-model.save_pretrained('path_to_save_model')
-tokenizer.save_pretrained('path_to_save_model')
+model.summary()
+
+model.save_weights(model_path)
