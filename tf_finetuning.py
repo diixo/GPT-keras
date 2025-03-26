04173bef6fb9672c690a7bab7bde3cca9f81139b
diixo <diixo.com@gmail.com>
2025-03-26 02:36:31 +0200

    tf_finetuning.py: initial commit

---
+++ tf_finetuning.py
@@ -0,0 +1,129 @@
+
+# https://www.kaggle.com/code/vimalpillai/finetuning-gpt2-model-tensorflow/notebook
+
+from tokenizers.models import BPE
+from tokenizers import Tokenizer
+from tokenizers.decoders import ByteLevel as ByteLevelDecoder
+from tokenizers.normalizers import Sequence, Lowercase
+from tokenizers.pre_tokenizers import ByteLevel
+from tokenizers.trainers import BpeTrainer
+from transformers import GPT2TokenizerFast, GPT2Config, TFGPT2LMHeadModel
+import tensorflow as tf
+import numpy as np
+
+
+# ---------- hyperparams ----------
+batch_size = 32
+seq_length = 64
+embedding_dim = 256
+dff = 256
+num_heads = 4
+num_layers = 4
+# ---------------------------------
+
+
+tokenizer = Tokenizer(BPE())
+tokenizer.normalizer = Sequence([Lowercase()])
+tokenizer.pre_tokenizer = ByteLevel()
+tokenizer.decoder = ByteLevelDecoder()
+
+trainer = BpeTrainer(vocab_size=50000, initial_alphabet=ByteLevel.alphabet(),special_tokens=[
+    "<a>","<pad>","</s>","<unk>","<mask>"
+    ])
+
+tokenizer.train(["austen-emma.txt"],trainer)
+
+tokenizer.save("tokenizer-gpt/tokenizer.json")
+
+tokenizer_gpt = GPT2TokenizerFast.from_pretrained("tokenizer-gpt")
+
+tokenizer_gpt.add_special_tokens({
+    "eos_token": "</s>",
+    "bos_token": "<s>",
+    "unk_token": "<unk>",
+    "pad_token": "<pad>",
+    "mask_token": "<mask>"
+})
+
+print("bos_token_id =", tokenizer_gpt.bos_token_id)
+
+tokenizer_gpt.encode("<s> This  is </s>")
+
+config = GPT2Config(
+    vocab_size=tokenizer_gpt.vocab_size,
+    bos_token_id=tokenizer_gpt.bos_token_id,
+    eos_token_id=tokenizer_gpt.eos_token_id
+)
+model = TFGPT2LMHeadModel(config)
+
+# Text Data Preprocessing #################################
+with open("austen-emma.txt", "r", encoding='utf-8') as f:
+    content = f.readlines()
+
+print("lines=", len(content))
+
+content_p = []
+for c in content:
+    if len(c)>10:
+        content_p.append(c.strip())
+content_p = " ".join(content_p)+tokenizer_gpt.eos_token
+
+tokenized_content = tokenizer_gpt.encode(content_p)
+
+print("tokenized_content=", len(tokenized_content))
+
+sample_len = 100
+examples = []
+for i in range(0, len(tokenized_content)):
+    examples.append(tokenized_content[i: i + sample_len])
+
+##################### prepare train-data #####################
+train_data = []
+labels = []
+for example in examples:
+    if len(example) == 100:
+        train_data.append(example[:-1])
+        labels.append(example[1:])
+
+train_data = np.array(train_data).astype(np.int32)
+labels = np.array(labels).astype(np.int32)
+
+buffer = 5000
+batch_size = 12
+dataset = tf.data.Dataset.from_tensor_slices((train_data,labels))
+
+dataset = dataset.shuffle(buffer).batch(batch_size, drop_remainder=True)
+
+print(len(dataset))
+
+
+# Defining Model optimizer, loss metrics and compiling Model ###################################
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4, epsilon=1e-08, clipnorm=1.0)
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
+model.compile(optimizer=optimizer, loss=[loss, *[None] * model.config.n_layer], metrics=[metric])
+
+# Fine Tuning the GPT2 Model ###################################################################
+
+epochs = 2
+model.fit(dataset, epochs=epochs)
+
+# Making Prediction and Saving Model ###########################################################
+
+def generate_text(start, model):
+    input_token_ids = tokenizer_gpt.encode(start, return_tensors='tf')
+    output = model.generate(input_token_ids,max_length = 200,
+            num_beams = 5,
+            temperature = 0.7,
+            no_repeat_ngram_size=2,
+            num_return_sequences=1
+            )
+    return tokenizer_gpt.decode(output[0])
+
+
+model.save_weights("fine_tuned_gpt.h5")
+
+model.save("my_gpt2")
+
+generate_text(" ", model)
