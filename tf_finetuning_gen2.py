2de8e1df5896186c215cd76b01ad44e511a8a9d0
diixo <diixo.com@gmail.com>
2025-03-29 01:22:51 +0200

    tf_finetuning_gen2.py: fixed to correct implementation text generation. Looks like finished

--- tf_finetuning_gen2.py
+++ tf_finetuning_gen2.py
@@ -7,7 +7,7 @@ from tokenizers.decoders import ByteLevel as ByteLevelDecoder
 from tokenizers.normalizers import Sequence, Lowercase
 from tokenizers.pre_tokenizers import ByteLevel
 from tokenizers.trainers import BpeTrainer
-from transformers import GPT2TokenizerFast, GPT2Config, TFGPT2LMHeadModel
+from transformers import GPT2TokenizerFast, GPT2Config, TFGPT2LMHeadModel, GenerationConfig
 import tensorflow as tf
 import numpy as np
 from pathlib import Path
@@ -17,14 +17,14 @@ import re
 # ---------- hyperparams ----------
 batch_size = 32
 seq_length = 32
-embedding_dim = 256
-dff = 256
+embedding_dim = 128
+dff = 128
 num_heads = 4
-num_layers = 4
+num_layers = 3
 # ---------------------------------
 
-epochs = 20
-learning_rate = 5e-4
+epochs = 10
+learning_rate = 3e-4
 
 model_path      = "tokenizer-gpt/tf-finetuning-gen2.h5"
 tokenizer_path  = "tokenizer-gpt/tokenizer.json"
@@ -36,6 +36,29 @@ def str_tokenize_words(s: str, stopwords=set()):
     if words: return [w for w in words if w not in stopwords]
     return []
 
+
+def clean_mask_tokens(encodings, mask_tokens, pad_token_id):
+
+    train_data = encodings["input_ids"]
+    attention_masks = encodings["attention_mask"]
+
+    batch_size, seq_len = train_data.shape
+    new_train_data = np.full((batch_size, seq_len), pad_token_id, dtype=np.int32)
+    new_attention_mask = np.zeros((batch_size, seq_len), dtype=np.int32)
+
+    for i in range(batch_size):
+        j = 0
+        for k in range(seq_len):
+            if train_data[i, k] in mask_tokens:
+                continue
+            if j < seq_len:
+                new_train_data[i, j] = train_data[i, k]
+                new_attention_mask[i, j] = attention_masks[i, k]
+                j += 1
+    encodings["input_ids"] = new_train_data
+    encodings["attention_mask"] = new_attention_mask
+
+# ---------------------------------
 with open("tokenizer-gpt/austen-emma.txt", "r", encoding='utf-8') as f:
     content = f.readlines()
 
@@ -68,12 +91,22 @@ tokenizer_gpt.add_special_tokens({
 
 # Text Data Preprocessing #################################
 
+mask_tokens_ids = tokenizer_gpt.convert_tokens_to_ids(['a', 'Ġa', 'an', 'Ġan', 'the', 'Ġthe'])
+
+mask_tokens = tokenizer_gpt.convert_ids_to_tokens(mask_tokens_ids)
+
+##########################################################################################
+
 encodings = tokenizer_gpt(content, padding="max_length", truncation=True, max_length=seq_length, return_tensors="np")
+
+clean_mask_tokens(encodings, set(mask_tokens_ids), tokenizer_gpt.pad_token_id)
 
 
 train_data = encodings["input_ids"][:, :-1]
 labels = encodings["input_ids"][:, 1:]
 attention_masks = encodings["attention_mask"][:, :-1]
+
+##########################################################################################
 
 ds_tf = tf.data.Dataset.from_tensor_slices((train_data, labels, attention_masks))
 dataset = ds_tf.shuffle(5000).batch(batch_size, drop_remainder=True)
@@ -92,14 +125,17 @@ config = GPT2Config(
 
     vocab_size=tokenizer_gpt.vocab_size,
     bos_token_id=tokenizer_gpt.bos_token_id,
-    eos_token_id=tokenizer_gpt.eos_token_id
+    eos_token_id=tokenizer_gpt.eos_token_id,
+    pad_token_id=tokenizer_gpt.pad_token_id
 )
 
 model = TFGPT2LMHeadModel(config)
 
-model.config.pad_token_id = tokenizer_gpt.pad_token_id
-model.config.bos_token_id = tokenizer_gpt.bos_token_id
-model.config.eos_token_id = tokenizer_gpt.eos_token_id
+print("model.config:",
+    model.config.pad_token_id,
+    model.config.bos_token_id,
+    model.config.eos_token_id
+    )
 
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, epsilon=1e-08, clipnorm=1.0)
@@ -128,20 +164,24 @@ def generate_text(prompt: str, model: TFGPT2LMHeadModel, max_length = seq_length
 
     encodings = tokenizer_gpt([prompt], return_tensors='tf')
 
-    input_token_ids = encodings['input_ids']
-    attention_mask = encodings['attention_mask']
+    gen_config = GenerationConfig(
+        max_length = max_length,
+        do_sample = True,
+        temperature = 0.9,
+        top_k = 30,
+        top_p = 0.9,
+        repetition_penalty = 1.2,
+        no_repeat_ngram_size = 1
+    )
 
     output = model.generate(
-        input_token_ids,
-        attention_mask = attention_mask,
-        max_length = max_length,
-        num_beams = 5,
-        do_sample = False,
-        no_repeat_ngram_size = 2,
-        num_return_sequences = 1
-        )
+        inputs = encodings['input_ids'],
+        attention_mask = encodings['attention_mask'],
+        generation_config = gen_config
+    )
+    #print(tokenizer_gpt.pad_token_id, tokenizer_gpt.bos_token_id, tokenizer_gpt.eos_token_id)
     # use add_special_tokens=True, because we use padding as special symbol
     return tokenizer_gpt.decode(output[0], skip_special_tokens=True)
 
 
-print(generate_text("Emma was", model))
+print(generate_text("Emma knew", model))
