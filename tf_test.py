c03d67cbf046800651ba4aebf3cf6a222f49f9d0
diixo <diixo.com@gmail.com>
2025-04-04 19:54:03 +0300

    tf_test.py: fixed shaping errors, sizing, replaced AutoTokenizer to GPT2TokenizerFast

--- tf_test.py
+++ tf_test.py
@@ -1,28 +1,30 @@
-from transformers import GPT2Tokenizer, TFGPT2LMHeadModel, GPT2Config, AutoTokenizer
+from transformers import GPT2Tokenizer, TFGPT2LMHeadModel, GPT2Config, AutoTokenizer, GPT2TokenizerFast
 import tensorflow as tf
 from pathlib import Path
 import re
 
+
+# ---------- hyperparams ----------
+batch_size = 64
+seq_length = 32
+embedding_dim = 256
+dff = 256
+num_heads = 8
+num_layers = 4
+# ---------------------------------
+
+learning_rate = 5e-4
+epochs = 5
+
+# ---------------------------------
 
 def str_tokenize_words(s: str, stopwords=set()):
     words = re.findall("(\.?\w[\w'\.&]*\w|\w\+*#?)", s)
     if words: return [w for w in words if w not in stopwords]
     return []
 
-# ---------- hyperparams ----------
-batch_size = 32
-seq_length = 32
-embedding_dim = 256
-dff = 256
-num_heads = 4
-num_layers = 4
-# ---------------------------------
 
-learning_rate = 5e-4
-epochs = 3
-
-
-model_path = f"shakespeare-gpt2-{embedding_dim}-{batch_size}-{seq_length}-{dff}-{num_heads}.h5"
+model_path = f"emma-gpt2-{embedding_dim}-{batch_size}-{seq_length}-{dff}-{num_heads}.h5"
 
 with open("tokenizer-gpt/austen-emma.txt", "r", encoding="utf-8") as file:
     lines = file.read().splitlines()
@@ -34,25 +36,37 @@ print(f"Lines: {len(lines)}, Batches per epoch: {batches_per_epoch}")
 
 #####################################################################
 
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
 tokenizer.pad_token = tokenizer.eos_token
 
+print(f"model.config: vocab.sz={tokenizer.vocab_size},",
+    f"pad_token_id={tokenizer.pad_token_id},",
+    f"bos_token_id={tokenizer.bos_token_id},",
+    f"eos_token_id={tokenizer.eos_token_id};",
+    )
 
-tokens = tokenizer(lines, padding=True, truncation=True, return_tensors='tf', max_length=seq_length)
+config = GPT2Config(
+    vocab_size=tokenizer.vocab_size, 
+    n_positions=seq_length + 1,
+    n_embd=embedding_dim, 
+    n_layer=num_layers, 
+    n_head=num_heads, 
+    n_inner=dff,
+
+    bos_token_id=tokenizer.bos_token_id,
+    eos_token_id=tokenizer.eos_token_id,
+    pad_token_id=tokenizer.pad_token_id
+)
+
+##########################################################################################
+
+tokens = tokenizer(lines, padding=True, truncation=True, return_tensors='np', max_length=seq_length + 1)
 
 input_ids = tokens["input_ids"]
 attention_masks = tokens["attention_mask"]
 
-
-config = GPT2Config(
-    vocab_size=tokenizer.vocab_size, 
-    n_positions=seq_length,
-    n_embd=embedding_dim, 
-    n_layer=num_layers, 
-    n_head=num_heads, 
-    n_inner=dff
-)
-
+##########################################################################################
 
 model = TFGPT2LMHeadModel(config)
 
@@ -66,9 +80,9 @@ def compute_loss(labels, logits):
 
 
 def map_fn(input_chunk, attention_mask):
-    input_chunk = input_chunk[:-1]
-    target_chunk = input_chunk[1:]
-    attention_mask = attention_mask[:-1]
+    input_chunk = input_chunk[:, :-1]
+    target_chunk = input_chunk[:, 1:]
+    attention_mask = attention_mask[:, :-1]
     return input_chunk, target_chunk, attention_mask
 
 
@@ -77,6 +91,7 @@ dataset = ds_tf.shuffle(5000).batch(batch_size, drop_remainder=True)
 
 model.compile(optimizer=optimizer, loss=compute_loss)
 
+###################################################
 
 if Path(model_path).exists():
     dummy_input = tf.ones((1, seq_length), dtype=tf.int32)
